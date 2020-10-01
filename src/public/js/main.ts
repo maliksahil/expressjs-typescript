232f16d6772badd8961b65795817e2feb0926846
Sahil Malik <sahilmalik@winsmarts.com>
2020-10-01 02:37:19 -0400

    Added vue

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,1 +1,24 @@
-console.log('client side scripting support');
+import Vue from "vue";
+
+// // tslint:disable no-unused-expression
+new Vue({ // eslint-disable-line no-new
+    computed: {
+    },
+    data() {
+        return {
+            todos: []
+        }
+    },
+    el: "#app",
+    methods: {
+
+    },
+    mounted() {
+        this.todos = [
+            { "id": 1, "title": "Integrate Vue.js", "isDone": true },
+            { "id": 2, "title": "Refactor this later", "isDone": false },
+            { "id": 3, "title": "Write an API", "isDone": false },
+            { "id": 4, "title": "Get data from a database", "isDone": false }
+        ];
+    }
+});
