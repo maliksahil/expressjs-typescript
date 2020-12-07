fbfd25041e40a33a614539097ca99f1dc9be1517
Sahil Malik <sahilmalik@winsmarts.com>
2020-12-07 00:45:49 -0500

    Fixed hostname issue for db

--- db/setupdb.sh
+++ db/setupdb.sh
@@ -1,2 +1,3 @@
 docker build --tag tododb:latest .
+docker container rm $(docker container ls -af name=tododb -q)
 docker run -it --name tododb -p 5432:5432 tododb
--- scripts/runindocker.sh
+++ scripts/runindocker.sh
@@ -1,3 +1,3 @@
 docker build --tag nodeapp:latest .
 docker container rm $(docker container ls -af name=nodeapp -q)
-docker run -it --name nodeapp -p 8080:8080 nodeapp
+docker run -it --name nodeapp -e PGHOST=$(hostname) -p 8080:8080 nodeapp
